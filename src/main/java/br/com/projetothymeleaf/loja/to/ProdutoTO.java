8e261b16b0c052abec1a5b4ddc834ed2ba8a81a0
gustavoaquino <guuaquino@hotmail.com>
2021-05-04 22:43:16 -0300

    Criação de ProjetoTO.

---
+++ src/main/java/br/com/projetothymeleaf/loja/to/ProdutoTO.java
@@ -0,0 +1,43 @@
+package br.com.projetothymeleaf.loja.to;
+
+import java.util.Date;
+
+public class ProdutoTO{
+
+    private Long id;
+    private String nome;
+    private Date dataCriacao;
+    private String valor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+}
